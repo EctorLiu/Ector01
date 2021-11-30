336bca12e8893e5327fc1291b05761a493600ca1
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2021-11-30 08:28:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ.get("30dJB3ONZ69bAmhdUmUGhNnkKeqC2brlk3X+qPx47PTQDgGRHrfrB2G/uPEOR0LVnHill36ctpNPYi840Tig+p0dHSJa/AOnwZLFTbR0O3cl4dOodKeF6J+bmv3zG9bf1FoU4D118ZLI5MDRIUBBsQdB04t89/1O/w1cDnyilFU="))
-handler = WebhookHandler(os.environ.get("fb7e1eb7a17d49714f35ebb6172f5153"))
+line_bot_api = LineBotApi(os.environ.get("1"))
+handler = WebhookHandler(os.environ.get("2"))
 
 
 @app.route("/", methods=["GET", "POST"])
