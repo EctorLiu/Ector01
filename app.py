2ec823b6ef9eafafdd399d39882b750aa2b4b6c4
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2021-12-23 08:10:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def handle_message(event):
     
     elif temp_message.count('Ver') > 0:
         # (Z)Ver
-        get_message = '『臺南市新吉工業區廠協會』版本：\n(LC22)2235'
+        get_message = '『臺南市新吉工業區廠協會』版本：\n(LC23)0810'
 
     ###################################################
 
