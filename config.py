63b962d92e1b399dcec6f4ff84325faab94dd8a4
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2021-12-27 11:26:56 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# ===== ===== ===== ===== ===== 【宣告區域】 ===== ===== ===== ===== =====
+import os
+
+# line-bot
+# channel_access_token = 'CHANNEL_ACCESS_TOKEN' 
+# channel_secret = 'CHANNEL_SECRET'
+strchannel_access_token = os.environ.get("CHANNEL_ACCESS_TOKEN")
+strchannel_secret = os.environ.get("CHANNEL_SECRET")
