230919b8f3e58922b49738f74585cc64d32a11c9
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2022-01-22 14:17:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -425,7 +425,7 @@ def handle_message(event):
             ms = MSSQL(host=GVstr254_host, port=GVstr254_port, user=GVstr254_user, pwd=GVstr254_pwd, db=GVstr254_TIM_DB)
             strSQL = ' SELECT SJMBCode, SJMBPRType, SJMBCorpUniNum, SJMBCorpName, SJMBPRName, ' + \
                         ' SJMBPRTitle, SJMBCorpAddress, SJMBCorpEmpNum, SJMBCorpProd, SJMBCorpTel, ' + \
-                        ' SJMBCorpSince ' + \
+                        ' SJMBCorpSince, SJMBCorpPRName, SJMBCorpPRTitle ' + \
                         ' FROM [TIM_DB].[dbo].[VIEW_0A_SJ_MemList] ' + \
                         ' WHERE [SJMBCorpName] LIKE ' + '\'%' + strCond + '%\'' + \
                         ' ORDER BY SEQ_TYPE, SJMBCode '
