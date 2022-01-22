e6bf7c0e177b807ae93661c9e5241e5e03c7a3d9
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2022-01-22 12:29:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # ===== ===== ===== ===== ===== 【宣告區域】 ===== ===== ===== ===== =====
 
     ##### 版本 ######
-strVer = '(M122)1220'
+strVer = '(M122)1225'
 
     # 切換【SQL】功能選擇：ON/OFF
 strSQL_FW_Switch = 'ON'
@@ -303,11 +303,11 @@ def handle_message(event):
                             '  (' + str(SJMBCorpUniNum) + ') ' + str(SJMBCorpName) + '\n' + \
                             '  代表：' + str(SJMBPRName) + ' ' + str(SJMBPRTitle) + '\n' + \
                             '  廠址：' + str(SJMBCorpAddress) + SJMBCorpProd + '\n\n'
+            if len(strTemp) >= 2000:
+                strTemp = strTemp[0:2000] + '...(資料過多)'
             get_message = strTitle + '：\n資料筆數[ ' + str(intCount) + ' ]\n' + len(strTemp) + \
                             '查詢時間：' + datNow  + '\n\n' + \
                             strTemp
-            if len(strTemp) >= 4000:
-                strTemp = strTemp[0:4000] + '...(資料過多)'
         else:
             get_message = strTitle + '：\n' + \
                             '目前ECTOR關閉防火牆\n' + \
