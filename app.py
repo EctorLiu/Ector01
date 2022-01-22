530dcbe0053783e309e33da85386dc40294712ab
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2022-01-22 14:16:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -424,22 +424,29 @@ def handle_message(event):
         if strSQL_FW_Switch == 'ON':
             ms = MSSQL(host=GVstr254_host, port=GVstr254_port, user=GVstr254_user, pwd=GVstr254_pwd, db=GVstr254_TIM_DB)
             strSQL = ' SELECT SJMBCode, SJMBPRType, SJMBCorpUniNum, SJMBCorpName, SJMBPRName, ' + \
-                        ' SJMBPRTitle, SJMBCorpAddress, SJMBCorpEmpNum ' + \
+                        ' SJMBPRTitle, SJMBCorpAddress, SJMBCorpEmpNum, SJMBCorpProd, SJMBCorpTel, ' + \
+                        ' SJMBCorpSince ' + \
                         ' FROM [TIM_DB].[dbo].[VIEW_0A_SJ_MemList] ' + \
                         ' WHERE [SJMBCorpName] LIKE ' + '\'%' + strCond + '%\'' + \
                         ' ORDER BY SEQ_TYPE, SJMBCode '
             resList = ms.RS_SQL_ExecQuery(strSQL)
             intCount=0
             strTemp=''
-            for (SJMBCode, SJMBPRType, SJMBCorpUniNum, SJMBCorpName, SJMBPRName, SJMBPRTitle, SJMBCorpAddress, SJMBCorpEmpNum) in resList:
+            for (SJMBCode, SJMBPRType, SJMBCorpUniNum, SJMBCorpName, SJMBPRName, \
+                    SJMBPRTitle, SJMBCorpAddress, SJMBCorpEmpNum, SJMBCorpProd, SJMBCorpTel, \
+                    SJMBCorpSince, SJMBCorpPRName, SJMBCorpPRTitle) in resList:
                 intCount += 1
                 strTemp += '[ ' + str(intCount) + ' ] 編號 【' + str(SJMBCode) + '】 ' + str(SJMBPRType) + ' (' + str(SJMBCorpEmpNum) + '人)\n' + \
                             '  (' + str(SJMBCorpUniNum) + ') ' + str(SJMBCorpName) + '\n' + \
                             '  ' + str(SJMBPRName) + ' ' + str(SJMBPRTitle) + '\n' + \
-                            '  廠址：' + str(SJMBCorpAddress) + '\n\n'
+                            '  成立：' + str(SJMBCorpSince) + '\n'
+                            '  廠址：' + str(SJMBCorpAddress) + '\n'
+                            '  電話：' + str(SJMBCorpTel) + '\n'
+                            '  公司負責人：' + str(SJMBCorpPRName) + ' ' + str(SJMBCorpPRTitle) + '\n'
+                            '  > 營業項目：' + str(SJMBCorpProd) + ' <\n\n'
             if len(strTemp) >= intMaxLineMSGString:
                 strTemp = strTemp[0:intMaxLineMSGString] + '...(資料過多)'
-            get_message = strTitle + '(' + str(len(strTemp)) + ')：\n' + \
+            get_message = strTitle + '：\n' + \
                             '資料筆數：[ ' + str(intCount) + ' ] \n' + \
                             '查詢時間：' + datNow  + '\n\n' + \
                             strTemp
