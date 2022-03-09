4c1b3770e623a0a338f9b56b4e038f363055da98
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2022-03-09 17:55:51 +0800

    Create rm_initial.py

---
+++ rm_initial.py
@@ -0,0 +1,21 @@
+    ##### 權杖資料 ######
+    # strEctorToken、strJohnboToken、strGwenToken、strKunToken、strMichelleToken
+# EctorLiu權杖：
+strEctorToken = 'fz684r2WIaxMU3PCZ3nKaTDoiyFVkCNezGXHDyaiBUg'
+# 智弘權杖：
+strJohnboToken = 'y1hnAMWKYk5jKU2flBQO5JRyTvpDTTOJMAVpHRDVIqC'
+# 冠伶權杖：
+strGwenToken = 'Nl4oSAcKqwQnhL1hJpsjsDiVvSUaEZTDmkuthCkENLN'
+# 昆霖權杖：
+strKunToken = 'JtjXyNHfdDTaESv3YGErLukPLnjDG6096d1yhjoRwlM'
+# 宜庭權杖：        
+strMichelleToken = 'SmLwU8fGy4xJNKbFmlSEpcTklNuXVY7S7Gn6ijTeQSz'
+    # ***** ***** ***** ***** *****
+
+    ##### SQL參數 ######
+GVstr254_host = '211.23.242.222'
+GVstr254_port = '2255'
+GVstr254_user = 'sa'
+GVstr254_pwd = '00000'
+GVstr254_TIM_DB = 'TIM_DB'
+    # ***** ***** ***** ***** *****
