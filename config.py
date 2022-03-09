4826f3a6f701934865b65a6142a1b7dd901492e4
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2022-03-09 17:54:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -14,21 +14,6 @@ strchannel_secret = os.environ.get("CHANNEL_SECRET")
     # ***** ***** ***** ***** *****
 
 
-    ##### 權杖資料 ######
-    # strEctorToken、strJohnboToken、strGwenToken、strKunToken、strMichelleToken
-# EctorLiu權杖：
-strEctorToken = 'fz684r2WIaxMU3PCZ3nKaTDoiyFVkCNezGXHDyaiBUg'
-# 智弘權杖：
-strJohnboToken = 'y1hnAMWKYk5jKU2flBQO5JRyTvpDTTOJMAVpHRDVIqC'
-# 冠伶權杖：
-strGwenToken = 'Nl4oSAcKqwQnhL1hJpsjsDiVvSUaEZTDmkuthCkENLN'
-# 昆霖權杖：
-strKunToken = 'JtjXyNHfdDTaESv3YGErLukPLnjDG6096d1yhjoRwlM'
-# 宜庭權杖：        
-strMichelleToken = 'SmLwU8fGy4xJNKbFmlSEpcTklNuXVY7S7Gn6ijTeQSz'
-    # ***** ***** ***** ***** *****
-
-
     ##### 教學資料 ######
 GVstrLessonLearning = 'A1. 申請官方帳號：\n' + \
                     'https://manager.line.biz/\n' + \
