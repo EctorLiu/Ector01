9b732f917b08423e2bb72f9b2116fb570cf1416f
EctorLiu <95218057+EctorLiu@users.noreply.github.com>
2022-03-10 22:50:59 +0800

    Update rs_auto_pyclock.py

--- rs_auto_pyclock.py
+++ rs_auto_pyclock.py
@@ -4,6 +4,7 @@
 # 1450-1550
     # ***** ***** ***** ***** *****
 
+
     ##### 匯入函式庫 ######
 from datetime import datetime
 
@@ -15,82 +16,21 @@ import urllib.request
 
 
     ##### 自動執行程式 ######
-@sched.scheduled_job('cron', day_of_week='mon-fri',hour = 7,minute = 50)
+@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/25')
+
 def scheduled_job():
     url = 'https://ector01.herokuapp.com/'
     conn = urllib.request.urlopen(url)
     for key, value in conn.getheaders():
         print(key, value)
-    sched.start()
-@sched.scheduled_job('cron', day_of_week='mon-fri',hour = 8,minute = 20)
-def scheduled_job():
-    url = 'https://ector01.herokuapp.com/'
-    conn = urllib.request.urlopen(url)
-    for key, value in conn.getheaders():
-        print(key, value)
-    sched.start()
-@sched.scheduled_job('cron', day_of_week='mon-fri',hour = 8,minute = 50)
-def scheduled_job():
-    url = 'https://ector01.herokuapp.com/'
-    conn = urllib.request.urlopen(url)
-    for key, value in conn.getheaders():
-        print(key, value)
-    sched.start()
-@sched.scheduled_job('cron', day_of_week='mon-fri',hour = 11,minute = 50)
-def scheduled_job():
-    url = 'https://ector01.herokuapp.com/'
-    conn = urllib.request.urlopen(url)
-    for key, value in conn.getheaders():
-        print(key, value)
-    sched.start()
-@sched.scheduled_job('cron', day_of_week='mon-fri',hour = 12,minute = 20)
-def scheduled_job():
-    url = 'https://ector01.herokuapp.com/'
-    conn = urllib.request.urlopen(url)
-    for key, value in conn.getheaders():
-        print(key, value)
-    sched.start()
-@sched.scheduled_job('cron', day_of_week='mon-fri',hour = 12,minute = 50)
-def scheduled_job():
-    url = 'https://ector01.herokuapp.com/'
-    conn = urllib.request.urlopen(url)
-    for key, value in conn.getheaders():
-        print(key, value)
-    sched.start()
-@sched.scheduled_job('cron', day_of_week='mon-fri',hour = 13,minute = 20)
-def scheduled_job():
-    url = 'https://ector01.herokuapp.com/'
-    conn = urllib.request.urlopen(url)
-    for key, value in conn.getheaders():
-        print(key, value)
-    sched.start()
-@sched.scheduled_job('cron', day_of_week='mon-fri',hour = 13,minute = 50)
-def scheduled_job():
-    url = 'https://ector01.herokuapp.com/'
-    conn = urllib.request.urlopen(url)
-    for key, value in conn.getheaders():
-        print(key, value)
-    sched.start()
-@sched.scheduled_job('cron', day_of_week='mon-fri',hour = 14,minute = 50)
-def scheduled_job():
-    url = 'https://ector01.herokuapp.com/'
-    conn = urllib.request.urlopen(url)
-    for key, value in conn.getheaders():
-        print(key, value)
-    sched.start()
-@sched.scheduled_job('cron', day_of_week='mon-fri',hour = 15,minute = 20)
-def scheduled_job():
-    url = 'https://ector01.herokuapp.com/'
-    conn = urllib.request.urlopen(url)
-    for key, value in conn.getheaders():
-        print(key, value)
-    sched.start()
+
+sched.start()
     # ***** ***** ***** ***** *****
 
 
     ##### 備註區域 ######
 # 表示2017年3月22日17时19分07秒执行该程序
-# sched.add_job(my_job, 'cron', year=2017,month = 3,day = 22,hour = 17,minute = 19,second = 7)
+# sched.add_job(my_job, 'cron', year=2017,month = 03,day = 22,hour = 17,minute = 19,second = 07)
  
 # 表示任务在6,7,8,11,12月份的第三个星期五的00:00,01:00,02:00,03:00 执行该程序
 # sched.add_job(my_job, 'cron', month='6-8,11-12', day='3rd fri', hour='0-3')
@@ -101,7 +41,7 @@ def scheduled_job():
 # 表示每5秒执行该程序一次，相当于interval 间隔调度中seconds = 5
 # sched.add_job(my_job, 'cron',second = '*/5')
 
-# @sched.scheduled_job('cron', day_of_week = 'mon-fri', hour = 11, minute = 35, second = 5)
+# @sched.scheduled_job('cron', day_of_week = 'mon-fri', hour = 11, minute = 35, second = 05)
 # @sched.scheduled_job('cron', year = 2021, month = 12, day = 27, hour = 12, minute = 2, second = 30)
 
 # @sched.scheduled_job('cron', year = 2021, month = 12, day = 25, minute = 20)
